b83f792ea4e2771be8a78fb9941b0f86cdf864d2
Piotr <bartnic1@gmail.com>
2018-03-06 21:54:56 +0000

    Correction to previous commit: Added knexLookupPeople.js and add_pesron.js to use knex to interact with the SEQUEL database. Also removed a trail test.js file

--- test.js
+++
@@ -1,7 +0,0 @@
-var knex = require('knex')(require('./knexfile').development);
-
-newid = knex('famous_people').max('id').then(function(rows){
-  let thing = rows;
-  console.log(typeof thing[0].max);
-}).then(console.log(thing));
-// console.log(newid);
